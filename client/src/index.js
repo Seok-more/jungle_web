ae38b70d78c9704e0790d1056c4ada5702ce19eb
JSM <64595778+Seok-more@users.noreply.github.com>
2025-10-18 16:35:12 +0900

    Redux
    ~28

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promise from 'redux-promise';
+import { thunk } from 'redux-thunk';               // ✅ 수정: default → named imp
+import rootReducer from './_reducers';           // 예: ./_reducers/index.js
+
+// Redux DevTools 연동 (없으면 compose 사용)
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(promise, thunk))  // ✅ 미들웨어 적용
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>                          {/* ✅ react-redux (오타 주의) */}
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-   
